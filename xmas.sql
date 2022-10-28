a566d75906c30677014e44ed660f8e1180a3e58c
wonghye <dearhw117@naver.com>
2022-10-28 14:24:43 +0900

    1028

--- xmas.sql
+++ xmas.sql
@@ -8,3 +8,10 @@ show databases;
 use xmas;
 show tables;
 
+
+select * from member;
+select * from item order by reg_time desc;
+select * from item_img order by reg_time desc;
+select * from orders order by reg_time desc;
+select * from order_item order by reg_time desc;
+
