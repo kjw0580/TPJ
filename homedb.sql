baeaf73b3ce6f19be6dd141e46cee7248d053db8
wonghye <dearhw117@naver.com>
2022-11-01 15:19:24 +0900

    login

---
+++ homedb.sql
@@ -0,0 +1,18 @@
+--  db 생성
+create database homedb;
+
+-- shop db 권한 주기
+grant all privileges on homedb.* to root@localhost with grant option;
+
+show databases;
+use homedb;
+show tables;
+
+
+select * from member;
+select * from item order by reg_time desc;
+select * from item_img order by reg_time desc;
+select * from orders order by reg_time desc;
+select * from order_item order by reg_time desc;
+
+update member set role = 'ADMIN';
